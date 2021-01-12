784b406ab297cc5f5beacc2caadf08a0a0def90b
Federico Landini <landini@fit.vutbr.cz>
2021-01-12 09:51:35 +0100

    Create directory for feature extraction

--- VBx/extract.sh
+++ VBx/extract.sh
@@ -22,9 +22,12 @@ rm -f $TASKFILE
 
 mkdir -p $OUT_DIR/lists $OUT_DIR/xvectors $OUT_DIR/segments
 while IFS= read -r line; do
+	mkdir -p "$(dirname $OUT_DIR/lists/$line)"
     grep $line $FILE_LIST > $OUT_DIR/lists/$line".txt"
     OUT_ARK_FILE=$OUT_DIR/xvectors/$line.ark
     OUT_SEG_FILE=$OUT_DIR/segments/$line
+    mkdir -p "$(dirname $OUT_ARK_FILE)"
+    mkdir -p "$(dirname $OUT_SEG_FILE)"
     if [[ "$DEVICE" == "gpu" ]]; then
     	echo "python $DIR/predict.py --seg-len 144 --seg-jump 24 --model $MODEL --weights $WEIGHTS --gpus=\$($DIR/free_gpu.sh) $MDL_WEIGHTS --ndim 64 --embed-dim $EMBED_DIM --in-file-list $OUT_DIR/lists/$line".txt" --in-lab-dir $LAB_DIR --in-wav-dir $WAV_DIR --out-ark-fn $OUT_ARK_FILE --out-seg-fn $OUT_SEG_FILE" >> $TASKFILE
     else
