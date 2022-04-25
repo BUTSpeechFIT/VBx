d5b1354189ab50dc473d9b6d2aa56d5e7e5f944d
Federico Landini <landini@fit.vutbr.cz>
2022-04-25 15:55:18 +0200

    Update VB functions with a simplified version that produces the same results and where the notation matches the symbols in the paper.

---
+++ VBx/VBx.py
@@ -0,0 +1,174 @@
+#!/usr/bin/env python
+
+# Copyright 2021 Lukas Burget, Mireia Diez (burget@fit.vutbr.cz, mireia@fit.vutbr.cz)
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# Revision History
+#   L. Burget   20/1/2021 1:00AM - original version derived from the more
+#                                  complex VB_diarization.py avaiable at
+# https://github.com/BUTSpeechFIT/VBx/blob/e39af548bb41143a7136d08310765746192e34da/VBx/VB_diarization.py
+#
+
+import numpy as np
+from scipy.special import logsumexp
+
+
+def VBx(X, Phi, loopProb=0.9, Fa=1.0, Fb=1.0, pi=10, gamma=None, maxIters=10,
+        epsilon=1e-4, alphaQInit=1.0, ref=None, plot=False,
+        return_model=False, alpha=None, invL=None):
+    """
+    Inputs:
+    X           - T x D array, where columns are D dimensional feature vectors
+                  (e.g. x-vectors) for T frames
+    Phi         - D array with across-class covariance matrix diagonal.
+                  The model assumes zero mean, diagonal across-class and
+                  identity within-class covariance matrix.
+    loopProb    - Probability of not switching speakers between frames
+    Fa          - Scale sufficient statiscits
+    Fb          - Speaker regularization coefficient Fb controls the final number of speakers
+    pi          - If integer value, it sets the maximum number of speakers
+                  that can be found in the utterance.
+                  If vector, it is the initialization for speaker priors (see Outputs: pi)
+    gamma       - An initialization for the matrix of responsibilities (see Outputs: gamma)
+    maxIters    - The maximum number of VB iterations
+    epsilon     - Stop iterating, if the obj. fun. improvement is less than epsilon
+    alphaQInit  - Dirichlet concentraion parameter for initializing gamma
+    ref         - T dim. integer vector with per frame reference speaker IDs (0:maxSpeakers)
+    plot        - If set to True, plot per-frame marginal speaker posteriors 'gamma'
+    return_model- Return also speaker model parameter
+    alpha, invL - If provided, these are speaker model parameters used in the first iteration
+
+    Outputs:
+    gamma       - S x T matrix of responsibilities (marginal posteriors)
+                  attributing each frame to one of S possible speakers
+                  (S is defined by input parameter pi)
+    pi          - S dimensional column vector of ML learned speaker priors.
+                  This allows us to estimate the number of speaker in the
+                  utterance as the probabilities of the redundant speaker
+                  converge to zero.
+    Li          - Values of auxiliary function (and DER and frame cross-entropy
+                  between gamma and reference, if 'ref' is provided) over iterations.
+    alpha, invL - Speaker model parameters returned only if return_model=True
+
+    Reference:
+      Landini F., Profant J., Diez M., Burget L.: Bayesian HMM clustering of
+      x-vector sequences (VBx) in speaker diarization: theory, implementation
+      and analysis on standard tasks
+    """
+    """
+    The comments in the code refers to the equations from the paper above. Also
+    the names of variables try to be consistent with the symbols in the paper.
+    """
+
+    D = X.shape[1]  # feature (e.g. x-vector) dimensionality
+
+    if type(pi) is int:
+        pi = np.ones(pi)/pi
+
+    if gamma is None:
+        # initialize gamma from flat Dirichlet prior with
+        # concentration parameter alphaQInit
+        gamma = np.random.gamma(alphaQInit, size=(X.shape[0], len(pi)))
+        gamma = gamma / gamma.sum(1, keepdims=True)
+
+    assert(gamma.shape[1] == len(pi) and gamma.shape[0] == X.shape[0])
+
+    G = -0.5*(np.sum(X**2, axis=1, keepdims=True) + D*np.log(2*np.pi))  # per-frame constant term in (23)
+    V = np.sqrt(Phi)  # between (5) and (6)
+    rho = X * V  # (18)
+    Li = []
+    for ii in range(maxIters):
+        # Do not start with estimating speaker models if those are provided
+        # in the argument
+        if ii > 0 or alpha is None or invL is None:
+            invL = 1.0 / (1 + Fa/Fb * gamma.sum(axis=0, keepdims=True).T*Phi)  # (17) for all speakers
+            alpha = Fa/Fb * invL * gamma.T.dot(rho)  # (16) for all speakers
+        log_p_ = Fa * (rho.dot(alpha.T) - 0.5 * (invL+alpha**2).dot(Phi) + G)  # (23) for all speakers
+        tr = np.eye(len(pi)) * loopProb + (1-loopProb) * pi  # (1) transition probability matrix
+        gamma, log_pX_, logA, logB = forward_backward(log_p_, tr, pi)  # (19) gamma, (20) logA, (21) logB, (22) log_pX_
+        ELBO = log_pX_ + Fb * 0.5 * np.sum(np.log(invL) - invL - alpha**2 + 1)  # (25)
+        pi = gamma[0] + (1-loopProb)*pi * np.sum(np.exp(logsumexp(
+            logA[:-1], axis=1, keepdims=True) + log_p_[1:] + logB[1:] - log_pX_
+        ), axis=0)  # (24)
+        pi = pi / pi.sum()
+        Li.append([ELBO])
+
+        # if reference is provided, report DER, cross-entropy and plot figures
+        if ref is not None:
+            Li[-1] += [DER(gamma, ref), DER(gamma, ref, xentropy=True)]
+
+            if plot:
+                import matplotlib.pyplot
+                if ii == 0:
+                    matplotlib.pyplot.clf()
+                matplotlib.pyplot.subplot(maxIters, 1, ii+1)
+                matplotlib.pyplot.plot(gamma, lw=2)
+                matplotlib.pyplot.imshow(np.atleast_2d(ref),
+                                         interpolation='none', aspect='auto',
+                                         cmap=matplotlib.pyplot.cm.Pastel1,
+                                         extent=(0, len(ref), -0.05, 1.05))
+
+        if ii > 0 and ELBO - Li[-2][0] < epsilon:
+            if ELBO - Li[-2][0] < 0:
+                print('WARNING: Value of auxiliary function has decreased!')
+            break
+    return (gamma, pi, Li) + ((alpha, invL) if return_model else ())
+
+
+# Calculates Diarization Error Rate (DER) or per-frame cross-entropy between
+# reference (vector of per-frame zero based integer speaker IDs) and gamma
+# (per-frame speaker posteriors). If expected=False, gamma is converted into
+# hard labels before calculating DER. If expected=TRUE, posteriors in gamma
+# are used to calculated "expected" DER.
+def DER(q, ref, expected=True, xentropy=False):
+    from scipy.sparse import coo_matrix
+    from scipy.optimize import linear_sum_assignment
+    if not expected:  # replce probabilities in q by zeros and ones
+        q = coo_matrix((np.ones(len(q)), (range(len(q)), q.argmax(1)))).toarray()
+
+    ref_mx = coo_matrix((np.ones(len(ref)), (range(len(ref)), ref)))
+    err_mx = ref_mx.T.dot(-np.log(q+np.nextafter(0, 1)) if xentropy else -q)
+    min_cost = err_mx[linear_sum_assignment(err_mx)].sum()
+    return min_cost/float(len(ref)) if xentropy else (len(ref) + min_cost)/float(len(ref))
+
+
+def forward_backward(lls, tr, ip):
+    """
+    Inputs:
+        lls - matrix of per-frame log HMM state output probabilities
+        tr  - transition probability matrix
+        ip  - vector of initial state probabilities (i.e. starting in the state)
+    Outputs:
+        pi  - matrix of per-frame state occupation posteriors
+        tll - total (forward) log-likelihood
+        lfw - log forward probabilities
+        lfw - log backward probabilities
+    """
+    ltr = np.log(tr)
+    lfw = np.empty_like(lls)
+    lbw = np.empty_like(lls)
+    lfw[:] = -np.inf
+    lbw[:] = -np.inf
+    lfw[0] = lls[0] + np.log(ip)
+    lbw[-1] = 0.0
+
+    for ii in range(1, len(lls)):
+        lfw[ii] = lls[ii] + logsumexp(lfw[ii-1] + ltr.T, axis=1)
+
+    for ii in reversed(range(len(lls)-1)):
+        lbw[ii] = logsumexp(ltr + lls[ii+1] + lbw[ii+1], axis=1)
+
+    tll = logsumexp(lfw[-1], axis=0)
+    pi = np.exp(lfw + lbw - tll)
+    return pi, tll, lfw, lbw
--- VBx/vbhmm.py
+++ VBx/vbhmm.py
@@ -21,33 +21,25 @@
 # is applied using the AHC output as args.initialization.
 #
 # A detailed analysis of this approach is presented in
-# Federico Landini, J\'{a}n Profant, Mireia Diez, Luk\'{a}\v{s} Burget
-# Bayesian HMM clustering of x-vector sequences (VBx) in speaker diarization: 
-# Theory, implementation and analysis on standard tasks
-# Computer Speech & Language, 2021
-# A more thorough description and study of the VB-HMM with eigen-voice priors
-# approach for diarization is presented in
-# M. Diez, L. Burget, F. Landini, J. \v{C}ernock\'{y}
-# Analysis of Speaker Diarization based on Bayesian HMM with Eigenvoice Priors,
-# IEEE Transactions on Audio, Speech and Language Processing, 2019
+# F. Landini, J. Profant, M. Diez, L. Burget
+# Bayesian HMM clustering of x-vector sequences (VBx) in speaker diarization:
+# theory, implementation and analysis on standard tasks
+# Computer Speech & Language, 2022
 
 import argparse
 import os
 import itertools
 
-import fastcluster
 import h5py
 import kaldi_io
 import numpy as np
-from scipy.cluster.hierarchy import fcluster
-from scipy.spatial.distance import squareform
 from scipy.special import softmax
 from scipy.linalg import eigh
 
-from diarization_lib import read_xvector_timing_dict, l2_norm, cos_similarity, twoGMMcalib_lin, merge_adjacent_labels, \
-    mkdir_p
+from diarization_lib import read_xvector_timing_dict, l2_norm, \
+    cos_similarity, twoGMMcalib_lin, AHC, merge_adjacent_labels, mkdir_p
 from kaldi_utils import read_plda
-from VB_diarization import VB_diarization
+from VBx import VBx
 
 
 def write_output(fp, out_labels, starts, ends):
@@ -58,32 +50,48 @@ def write_output(fp, out_labels, starts, ends):
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('--init', required=True, type=str, choices=['AHC', 'AHC+VB', 'random_5'],
-                        help='AHC for using only AHC or AHC+VB for VB-HMM after AHC initilization or random_5 '
-                             'for running 5 random initializations for VBx and picking the best per-ELBO')
-    parser.add_argument('--out-rttm-dir', required=True, type=str, help='Directory to store output rttm files')
+    parser.add_argument('--init', required=True, type=str,
+                        choices=['AHC', 'AHC+VB'],
+                        help='AHC for using only AHC or AHC+VB for VB-HMM '
+                        'after AHC initilization', )
+    parser.add_argument('--out-rttm-dir', required=True, type=str,
+                        help='Directory to store output rttm files')
     parser.add_argument('--xvec-ark-file', required=True, type=str,
-                        help='Kaldi ark file with x-vectors from one or more input recordings. '
-                             'Attention: all x-vectors from one recording must be in one ark file')
+                        help='Kaldi ark file with x-vectors from one or more '
+                             'input recordings. Attention: all x-vectors from '
+                             'one recording must be in one ark file')
     parser.add_argument('--segments-file', required=True, type=str,
-                        help='File with x-vector timing info (see diarization_lib.read_xvector_timing_dict)')
+                        help='File with x-vector timing info. See '
+                        'diarization_lib.read_xvector_timing_dict')
     parser.add_argument('--xvec-transform', required=True, type=str,
                         help='path to x-vector transformation h5 file')
     parser.add_argument('--plda-file', required=True, type=str,
-                        help='File with PLDA model in Kaldi format used for AHC and VB-HMM x-vector clustering')
-    parser.add_argument('--threshold', required=True, type=float, help='args.threshold (bias) used for AHC')
+                        help='File with PLDA model in Kaldi format used for '
+                        'AHC and VB-HMM x-vector clustering')
+    parser.add_argument('--threshold', required=True, type=float,
+                        help='args.threshold (bias) used for AHC')
     parser.add_argument('--lda-dim', required=True, type=int,
-                        help='For VB-HMM, x-vectors are reduced to this dimensionality using LDA')
+                        help='For VB-HMM, x-vectors are reduced to this '
+                        'dimensionality using LDA')
     parser.add_argument('--Fa', required=True, type=float,
-                        help='Parameter of VB-HMM (see VB_diarization.VB_diarization)')
+                        help='Parameter of VB-HMM (see VBx.VBx)')
     parser.add_argument('--Fb', required=True, type=float,
-                        help='Parameter of VB-HMM (see VB_diarization.VB_diarization)')
+                        help='Parameter of VB-HMM (see VBx.VBx)')
     parser.add_argument('--loopP', required=True, type=float,
-                        help='Parameter of VB-HMM (see VB_diarization.VB_diarization)')
-    parser.add_argument('--init-smoothing', required=False, type=float, default=5.0,
-                        help='AHC produces hard assignments of x-vetors to speakers. These are "smoothed" to soft '
-                             'assignments as the args.initialization for VB-HMM. This parameter controls the amount of'
-                             ' smoothing. Not so important, high value (e.g. 10) is OK  => keeping hard assigment')
+                        help='Parameter of VB-HMM (see VBx.VBx)')
+    parser.add_argument('--target-energy', required=False, type=float,
+                        default=1.0,
+                        help='Parameter affecting AHC if the similarity '
+                             'matrix is obtained with PLDA. See '
+                             'diarization_lib.kaldi_ivector_plda_scoring_dense')
+    parser.add_argument('--init-smoothing', required=False, type=float,
+                        default=5.0,
+                        help='AHC produces hard assignments of x-vetors to '
+                             'speakers. These are "smoothed" to soft '
+                             'assignments as the args.initialization for '
+                             'VB-HMM. This parameter controls the amount of '
+                             'smoothing. Not so important, high value '
+                             '(e.g. 10) is OK  => keeping hard assigment')
     parser.add_argument('--output-2nd', required=False, type=bool, default=False,
                         help='Output also second most likely speaker of VB-HMM')
 
@@ -101,10 +109,11 @@ if __name__ == '__main__':
     plda_psi = acvar[::-1]
     plda_tr = wccn.T[::-1]
 
-    # Open ark file with x-vectors and in each iteration of the following for-loop
-    # read a batch of x-vectors corresponding to one recording
+    # Open ark file with x-vectors and in each iteration of the following
+    # for-loop read a batch of x-vectors corresponding to one recording
     arkit = kaldi_io.read_vec_flt_ark(args.xvec_ark_file)
-    recit = itertools.groupby(arkit, lambda e: e[0].rsplit('_', 1)[0]) # group xvectors in ark by recording name
+    # group xvectors in ark by recording name
+    recit = itertools.groupby(arkit, lambda e: e[0].rsplit('_', 1)[0])
     for file_name, segs in recit:
         print(file_name)
         seg_names, xvecs = zip(*segs)
@@ -116,20 +125,15 @@ if __name__ == '__main__':
             lda = np.array(f['lda'])
             x = l2_norm(lda.T.dot((l2_norm(x - mean1)).transpose()).transpose() - mean2)
 
-        if args.init == 'AHC' or args.init.endswith('VB') or args.init.startswith('random_'):
+        if args.init == 'AHC' or args.init.endswith('VB'):
             if args.init.startswith('AHC'):
                 # Kaldi-like AHC of x-vectors (scr_mx is matrix of pairwise
                 # similarities between all x-vectors)
                 scr_mx = cos_similarity(x)
                 # Figure out utterance specific args.threshold for AHC.
-                thr, _ = twoGMMcalib_lin(scr_mx.ravel())
+                thr, junk = twoGMMcalib_lin(scr_mx.ravel())
                 # output "labels" is an integer vector of speaker (cluster) ids
-                scr_mx = squareform(-scr_mx, checks=False)
-                lin_mat = fastcluster.linkage(scr_mx, method='average', preserve_input='False')
-                del scr_mx
-                adjust = abs(lin_mat[:, 2].min())
-                lin_mat[:, 2] += adjust
-                labels1st = fcluster(lin_mat, -(thr + args.threshold) + adjust, criterion='distance') - 1
+                labels1st = AHC(scr_mx, thr + args.threshold)
             if args.init.endswith('VB'):
                 # Smooth the hard labels obtained from AHC to soft assignments
                 # of x-vectors to speakers
@@ -137,44 +141,15 @@ if __name__ == '__main__':
                 qinit[range(len(labels1st)), labels1st] = 1.0
                 qinit = softmax(qinit * args.init_smoothing, axis=1)
                 fea = (x - plda_mu).dot(plda_tr.T)[:, :args.lda_dim]
-                # Use VB-HMM for x-vector clustering. Instead of i-vector extractor model, we use PLDA
-                # => GMM with only 1 component, V derived accross-class covariance,
-                # and iE is inverse within-class covariance (i.e. identity)
-                sm = np.zeros(args.lda_dim)
-                siE = np.ones(args.lda_dim)
-                sV = np.sqrt(plda_psi[:args.lda_dim])
-                q, sp, L = VB_diarization(
-                    fea, sm, np.diag(siE), np.diag(sV),
-                    pi=None, gamma=qinit, maxSpeakers=qinit.shape[1],
-                    maxIters=40, epsilon=1e-6, 
+                q, sp, L = VBx(
+                    fea, plda_psi[:args.lda_dim],
+                    pi=qinit.shape[1], gamma=qinit,
+                    maxIters=40, epsilon=1e-6,
                     loopProb=args.loopP, Fa=args.Fa, Fb=args.Fb)
 
                 labels1st = np.argsort(-q, axis=1)[:, 0]
                 if q.shape[1] > 1:
                     labels2nd = np.argsort(-q, axis=1)[:, 1]
-            if args.init.startswith("random_"):
-                MAX_SPKS = 10
-                prev_L = -float('inf')
-                random_iterations = int(args.init.split('_')[1])
-                np.random.seed(3) # for reproducibility
-                for _ in range(random_iterations):
-                    q_init = np.random.normal(size=(x.shape[0], MAX_SPKS),loc=0.5,scale=0.01)
-                    q_init = softmax(q_init * args.init_smoothing, axis=1)
-                    fea = (x - plda_mu).dot(plda_tr.T)[:, :args.lda_dim]
-                    sm = np.zeros(args.lda_dim)
-                    siE = np.ones(args.lda_dim)
-                    sV = np.sqrt(plda_psi[:args.lda_dim])
-                    q_tmp, sp, L = VB_diarization(
-                        fea, sm, np.diag(siE), np.diag(sV), 
-                        pi=None, gamma=q_init, maxSpeakers=q_init.shape[1], 
-                        maxIters=40, epsilon=1e-6, 
-                        loopProb=args.loopP, Fa=args.Fa, Fb=args.Fb)
-                    if L[-1][0] > prev_L:
-                        prev_L = L[-1][0]
-                        q = q_tmp
-                labels1st = np.argsort(-q, axis=1)[:, 0]
-                if q.shape[1] > 1:
-                    labels2nd = np.argsort(-q, axis=1)[:, 1]                    
         else:
             raise ValueError('Wrong option for args.initialization.')
 
